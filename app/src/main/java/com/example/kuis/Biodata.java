d54a7a9859fa10c28f99ebd62e2c48f79ab3d23e
bayu-17ajid <bayuajid12@gmail.com>
2021-04-01 13:47:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kuis/Biodata.java
@@ -0,0 +1,52 @@
+package com.example.kuis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Biodata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biodata);
+    }
+
+    public void back(View view) {
+        Intent b = new Intent(Biodata.this,MainActivity.class);
+        startActivity(b);
+    }
+
+    public void exit(View view) {
+        logout(this);
+    }
+
+    public static void logout(Activity activity){
+        AlertDialog.Builder builder= new AlertDialog.Builder(activity);
+        builder.setTitle("Keluar");
+        builder.setMessage("Serius Mau Keluar?");
+
+        builder.setPositiveButton("IYE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+
+        builder.setNegativeButton("NDAJI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+}
---
+++ app/src/main/java/com/example/kuis/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.kuis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Biodata(View view) {
+        Intent a = new Intent(MainActivity.this,Biodata.class);
+        startActivity(a);
+    }
+
+    public void moveMaps(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:-2.607150,120.552331")).setPackage("com.google.android.apps.maps"));
+    }
+
+    public void moveNumber(View view) {
+        String phoneNumber = "+6281355474184";
+        if (!phoneNumber.isEmpty()) {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + phoneNumber));
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+    }
+
+    public void exit(View view) {
+      logout(this);
+    }
+
+    public static void logout(Activity activity){
+        AlertDialog.Builder builder= new AlertDialog.Builder(activity);
+        builder.setTitle("Keluar");
+        builder.setMessage("Serius Mau Keluar?");
+
+        builder.setPositiveButton("IYE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+
+        builder.setNegativeButton("NDAJI", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+}
